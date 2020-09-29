32ea9ba632133981bc6060e08c78274e5ce7d3d5
bigfishh <anniezheng.anniee@gmail.com>
2020-09-29 12:07:29 -0400

    adds cors config

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module TheCodeBendingDictionary
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.api_only = true
   end
 end
