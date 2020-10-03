4b99590479288e89b6610a74449d6a851bfc6e4c
Eric Kim <hyeokjungkim@gmail.com>
2020-10-02 21:25:12 -0400

    validations to stop words from being associated

--- app/models/related_word.rb
+++ app/models/related_word.rb
@@ -4,7 +4,8 @@ class RelatedWord < ApplicationRecord
 
     # Validation so that a word can only be related to another only once
     validates :word_two_id, uniqueness: {scope: :word_one_id}
-    
+    validate :stop_self_relation
+
     # Documentation: https://collectiveidea.com/blog/archives/2015/07/30/bi-directional-and-self-referential-associations-in-rails
     after_create :create_inverse, unless: :has_inverse?
     after_destroy :destroy_inverses, if: :has_inverse?
@@ -25,6 +26,12 @@ class RelatedWord < ApplicationRecord
 
     def inverse_attributes
         { word_one_id: self.word_two_id, word_two_id: self.word_one_id }
+    end
+
+    def stop_self_relation
+        if word_one_id == word_two_id
+            errors.add(:word_one_id, "cannot be the same as word_two_id")
+        end
     end
 
 end
