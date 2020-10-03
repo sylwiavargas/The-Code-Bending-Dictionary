a3b106e5a1a6fc95ce5990da92f11d7e152fdebc
HyeokJungKim <31145202+HyeokJungKim@users.noreply.github.com>
2020-10-02 22:44:27 -0400

    Relatedwords dev (#37)
    * Dev (#29)
    
    * updates word (#26)
    
    * updates favorite (#27)
    
    * updates readme to include contribution guide (#28)
    
    * updates readme to include contribution guide
    
    * updates readme to include contribution guide
    
    * changes
    
    Co-authored-by: Annie Zheng <24644341+bigfishh@users.noreply.github.com>
    
    * change master to main
    
    * adds relatedWord
    
    * adds the associations in the models
    
    * validations to stop words from being associated
    
    Co-authored-by: Sylwia Vargas <45401242+sylwiavargas@users.noreply.github.com>
    Co-authored-by: Annie Zheng <24644341+bigfishh@users.noreply.github.com>

---
+++ app/models/related_word.rb
@@ -0,0 +1,37 @@
+class RelatedWord < ApplicationRecord
+    belongs_to :word_one, class_name: "Word"
+    belongs_to :word_two, class_name: "Word"
+
+    # Validation so that a word can only be related to another only once
+    validates :word_two_id, uniqueness: {scope: :word_one_id}
+    validate :stop_self_relation
+
+    # Documentation: https://collectiveidea.com/blog/archives/2015/07/30/bi-directional-and-self-referential-associations-in-rails
+    after_create :create_inverse, unless: :has_inverse?
+    after_destroy :destroy_inverses, if: :has_inverse?
+
+    private
+
+    def create_inverse
+        self.class.create(inverse_attributes)
+    end
+
+    def destroy_inverses
+        self.class.where(inverse_attributes).destroy_all
+    end
+
+    def has_inverse?
+        self.class.exists?(inverse_attributes)
+    end
+
+    def inverse_attributes
+        { word_one_id: self.word_two_id, word_two_id: self.word_one_id }
+    end
+
+    def stop_self_relation
+        if word_one_id == word_two_id
+            errors.add(:word_one_id, "cannot be the same as word_two_id")
+        end
+    end
+
+end
---
+++ db/migrate/20201002131844_create_related_words.rb
@@ -0,0 +1,10 @@
+class CreateRelatedWords < ActiveRecord::Migration[6.0]
+  def change
+    create_table :related_words do |t|
+      t.integer :word_one_id
+      t.integer :word_two_id
+
+      t.timestamps
+    end
+  end
+end
