a7089c8a7074d66237c438a1d3a0ee0ce357855d
Gerry <gerardo.larios69@gmail.com>
2020-10-04 09:38:50 -0500

    [ADD] vote controller with actions new, create, edit, update, destroy

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,34 @@
+class VotesController < ApplicationController
+  before_action :get_vote, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @vote = Vote.new
+  end 
+
+  def create
+    @vote = Vote.create(vote_params)
+  end
+
+  def edit;end
+
+  def update
+    @vote = Vote.create(vote_params)
+    redirect_to votes_path
+  end
+
+  def destroy
+    @vote.destroy
+    redirect_to votes_path
+  end
+
+  private
+
+  def get_vote
+    @user = Vote.find(params[:id])
+  end
+
+  def vote_params
+    values = params.require(:vote).permit(:value, :definition_id)
+    { user: @current_user, definition_id: values[:definition_id], value: values[:value] }
+  end
+end
